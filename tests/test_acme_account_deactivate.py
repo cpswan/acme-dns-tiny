fbcc5358fa580c57ad37e67c64271c01668d2c8d
Adrien Dorsaz <adrien@adorsaz.ch>
2020-05-22 20:57:40 +0200

    lint all tests

--- tests/test_acme_account_deactivate.py
+++ tests/test_acme_account_deactivate.py
@@ -1,45 +1,51 @@
-import unittest, os, time, configparser
+#!/usr/bin/env python3
+"""Test the acme_account_deactivate script with real ACME server"""
+import unittest
+import os
+import configparser
 import acme_dns_tiny
 from tests.config_factory import generate_acme_account_deactivate_config
 import tools.acme_account_deactivate
 
-ACMEDirectory = os.getenv("GITLABCI_ACMEDIRECTORY_V2", "https://acme-staging-v02.api.letsencrypt.org/directory")
+ACMEDIRECTORY = os.getenv("GITLABCI_ACMEDIRECTORY_V2",
+                          "https://acme-staging-v02.api.letsencrypt.org/directory")
 
 class TestACMEAccountDeactivate(unittest.TestCase):
     "Tests for acme_account_deactivate"
 
     @classmethod
-    def setUpClass(self):
-        self.configs = generate_acme_account_deactivate_config()
+    def setUpClass(cls):
+        cls.configs = generate_acme_account_deactivate_config()
         try:
-            acme_dns_tiny.main([self.configs['config']])
+            acme_dns_tiny.main([cls.configs['config']])
         except ValueError as err:
             if str(err).startswith("Error register"):
-                raise ValueError("Fail test as account has not been registered correctly: {0}".format(err))
+                raise ValueError("Fail test as account has not been registered correctly: {0}"
+                                 .format(err))
 
-        super(TestACMEAccountDeactivate, self).setUpClass()
+        super(TestACMEAccountDeactivate, cls).setUpClass()
 
     # To clean ACME staging server and close correctly temporary files
     @classmethod
-    def tearDownClass(self):
+    def tearDownClass(cls):
         # Remove temporary files
         parser = configparser.ConfigParser()
-        parser.read(self.configs['config'])
+        parser.read(cls.configs['config'])
         try:
             os.remove(parser["acmednstiny"]["AccountKeyFile"])
             os.remove(parser["acmednstiny"]["CSRFile"])
-            os.remove(self.configs['config'])
-        except:
+            os.remove(cls.configs['config'])
+        except: # pylint: disable=bare-except
             pass
-        super(TestACMEAccountDeactivate, self).tearDownClass()
+        super(TestACMEAccountDeactivate, cls).tearDownClass()
 
     def test_success_account_deactivate(self):
         """ Test success account key deactivate """
         with self.assertLogs(level='INFO') as accountdeactivatelog:
             tools.acme_account_deactivate.main(["--account-key", self.configs['key'],
-                                            "--acme-directory", ACMEDirectory])
+                                                "--acme-directory", ACMEDIRECTORY])
         self.assertIn("INFO:acme_account_deactivate:Account key deactivated !",
-            accountdeactivatelog.output)
+                      accountdeactivatelog.output)
 
 if __name__ == "__main__":
     unittest.main()
